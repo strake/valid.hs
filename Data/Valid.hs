efdcb6bb7d84107dabb92d3310720de12a1984ee
M Farkas-Dyck <strake888@gmail.com>
2019-08-22 15:41:59 -0800

    more features

--- Data/Valid.hs
+++ Data/Valid.hs
@@ -1,5 +1,11 @@
 module Data.Valid where
 
+import Data.Bifoldable
+import Data.Bifunctor
+import Data.Bitraversable
+import Data.Functor.Classes
+import Util (compose2)
+
 data Valid e a = Failure e | Valid a
   deriving (Eq, Read, Show, Functor, Foldable, Traversable)
 
@@ -9,3 +15,16 @@ instance Semigroup e => Applicative (Valid e) where
     Failure x <*> Valid   _ = Failure x
     Valid   _ <*> Failure y = Failure y
     Valid   f <*> Valid   x = Valid (f x)
+
+instance Bifunctor Valid where bimap = bimapDefault
+instance Bifoldable Valid where bifoldMap = bifoldMapDefault
+instance Bitraversable Valid where bitraverse f g = fmap fromEither . bitraverse f g . toEither
+
+instance Eq2 Valid where liftEq2 f g = compose2 (liftEq2 f g) toEither toEither
+
+fromEither :: Either e a -> Valid e a
+fromEither = either Failure Valid
+
+toEither :: Valid e a -> Either e a
+toEither (Failure e) = Left  e
+toEither (Valid   a) = Right a
